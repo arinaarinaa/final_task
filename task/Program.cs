c0216345a6785cc4ec6627a38a247acebb4866a9
arinach <arina.tchukurova@yandex.ru>
2022-09-29 01:22:32 +0300

    add firstvoid

---
+++ task/Program.cs
@@ -0,0 +1,12 @@
+﻿string [] ArrayCreation()
+{
+    
+    Console.WriteLine("Введите слова или символы через запятую - ");
+    char [] separators = new char[] {' ', ',', '-'};
+    string [] massive = Array.ConvertAll(Console.ReadLine().Split(separators),Convert.ToString);
+    Console.WriteLine();
+    Console.WriteLine("Ваш массив - ");
+    Console.Write(string.Join("| ", massive));
+    Console.WriteLine();
+    return massive;
+}
